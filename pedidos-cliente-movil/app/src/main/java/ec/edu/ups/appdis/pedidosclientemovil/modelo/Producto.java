ab5eca5668f61084e99f4e07095c88265e50f8d2
ctimbi <ctimbi@gmail.com>
2017-12-31 16:42:35 -0500

    Carga inicial app movil cliente Rest

---
+++ pedidos-cliente-movil/app/src/main/java/ec/edu/ups/appdis/pedidosclientemovil/modelo/Producto.java
@@ -0,0 +1,63 @@
+package ec.edu.ups.appdis.pedidosclientemovil.modelo;
+
+import java.io.Serializable;
+
+import java.math.BigDecimal;
+
+
+public class Producto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer productoid;
+
+	private String descripcion;
+
+	private Integer existencia;
+
+	private BigDecimal preciounit;
+
+	public Producto() {
+	}
+
+	public Integer getProductoid() {
+		return productoid;
+	}
+
+	public void setProductoid(Integer productoid) {
+		this.productoid = productoid;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Integer getExistencia() {
+		return existencia;
+	}
+
+	public void setExistencia(Integer existencia) {
+		this.existencia = existencia;
+	}
+
+	public BigDecimal getPreciounit() {
+		return preciounit;
+	}
+
+	public void setPreciounit(BigDecimal preciounit) {
+		this.preciounit = preciounit;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto{" +
+				"productoid=" + productoid +
+				", descripcion='" + descripcion + '\'' +
+				", existencia=" + existencia +
+				", preciounit=" + preciounit +
+				'}';
+	}
+}
